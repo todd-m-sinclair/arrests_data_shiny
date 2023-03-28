be11889812cf692310eb2cbb6d2fcb015508ccdd
todd-m-sinclair <todd.m.sinclair@icloud.com>
2023-03-28 12:29:41 +0900

    Prepare histogram, boxplot, and choices for selectInput without states column

--- global.R
+++ global.R
@@ -1,6 +1,7 @@
 # Helper file, runs at launch. Accessible to both ui.R and server.R objects
 
 library(tidyverse)
+library(plotly)
 # USArrests dataset
 
 my_data <- USArrests
@@ -21,6 +22,31 @@ my_data %>%
 states=rownames(my_data)
 
 #Add column to data set
-my_data = my_data %>% mutate(State=states)
+my_data = my_data %>% 
+  mutate(State=states)
 
 str(my_data)
+
+# Second menuItem visualization
+# Create Histogram and boxplot for distribution tabPanel
+
+# histogram
+p1 <- my_data %>%
+  plot_ly() %>%
+  add_histogram(~Rape) %>%
+  layout(xaxis = list(title="Rape"))
+
+# box plot
+p2 <- my_data %>%
+  plot_ly() %>%
+  add_boxplot(~Rape) %>%
+  layout(yaxis = list(showticklabels=F))
+
+#stack plots
+subplot(p2, p1, nrows =2, shareX = TRUE) %>% hide_legend() %>%
+  layout(title="Distribution chart - Histogram and Boxplot", 
+         yaxis = list(title="Frequency"))
+
+
+#Choices for selectInput - without States column
+c1 = my_data %>% select(-State) %>% names
