9b295320d8fcb25f306184215cf8a24a1f735f9b
todd-m-sinclair <todd.m.sinclair@icloud.com>
2023-03-28 10:24:43 +0900

    Add sidebarmenu and sidebarmenu items.

--- ui.R
+++ ui.R
@@ -7,23 +7,24 @@ dashboardPage(
                   titleWidth = 650,
                   tags$li(class = "dropdown", 
                           tags$a(href="https://www.youtube.com/watch?v=tlOBVZx8Hy0&list=PL6wLL_RojB5xNOhe2OTSd-DPkMLVY9DfB&index=76", 
-                                 icon("youtube"), 
-                                 "Tutorial", 
-                                 target="_blank")
-                          ),
+                                 icon("youtube"), "Tutorial", target="_blank")),
                   tags$li(class = "dropdown", 
                           tags$a(href="https://www.linkedin.com/in/toddsin/", 
-                                 icon("linkedin"), 
-                                 "My LinkedIn", 
-                                 target="_blank")
-                  ),
+                                 icon("linkedin"), "My LinkedIn", target="_blank")),
                   tags$li(class = "dropdown", 
                           tags$a(href="https://github.com/todd-m-sinclair", 
-                                 icon("github"), 
-                                 "My GitHub", 
-                                 target="_blank")
-                  )
+                                 icon("github"), "My GitHub", target="_blank"))
                   ),
-  dashboardSidebar(),
+  dashboardSidebar(
+    #sidebarmenu
+    sidebarMenu(
+      id = "sidebar",
+      
+      #first menuitem
+      menuItem("Dataset", tabname = "data", icon=icon("database")),
+      menuItem("visualization", tabname = "viz" , icon=icon("charl-line")),
+      menuItem("Choropleth Map", tabname ="map" , icon=icon("map"))
+    )
+  ),
   dashboardBody()
 )
