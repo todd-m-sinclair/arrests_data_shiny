adbf7f24807fbb274fb2db34d7a0df4376e0389a
todd-m-sinclair <todd.m.sinclair@icloud.com>
2023-03-28 11:52:27 +0900

    Add output objects for structure, summary and dataT

--- server.R
+++ server.R
@@ -1,5 +1,21 @@
 # Computation and reactivity
+library(DT)
 
 function(input, output, session){
+  #Structure
+  output$structure <-renderPrint(
+    my_data %>% 
+      str()
+  )
   
+  #Summary
+  output$summary <-renderPrint(
+    my_data %>% 
+      summary()
+  )
+  
+  #Datatable
+  output$dataT <-renderDataTable(
+    my_data
+  )
 }
