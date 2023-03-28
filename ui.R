fc67fef420441f9ee3d540339c7076a2efa52418
todd-m-sinclair <todd.m.sinclair@icloud.com>
2023-03-28 10:19:20 +0900

    Add social media links and icons

--- ui.R
+++ ui.R
@@ -4,7 +4,26 @@ library(shinydashboard)
 
 dashboardPage(
   dashboardHeader(title = "1973 US Arrests with R & Shiny Dashboard",
-                  titleWidth = 650),
+                  titleWidth = 650,
+                  tags$li(class = "dropdown", 
+                          tags$a(href="https://www.youtube.com/watch?v=tlOBVZx8Hy0&list=PL6wLL_RojB5xNOhe2OTSd-DPkMLVY9DfB&index=76", 
+                                 icon("youtube"), 
+                                 "Tutorial", 
+                                 target="_blank")
+                          ),
+                  tags$li(class = "dropdown", 
+                          tags$a(href="https://www.linkedin.com/in/toddsin/", 
+                                 icon("linkedin"), 
+                                 "My LinkedIn", 
+                                 target="_blank")
+                  ),
+                  tags$li(class = "dropdown", 
+                          tags$a(href="https://github.com/todd-m-sinclair", 
+                                 icon("github"), 
+                                 "My GitHub", 
+                                 target="_blank")
+                  )
+                  ),
   dashboardSidebar(),
   dashboardBody()
 )
