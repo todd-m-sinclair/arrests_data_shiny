3d8b323151de3ce5769c600caa8b0ceab6fb7617
todd-m-sinclair <todd.m.sinclair@icloud.com>
2023-03-28 13:04:05 +0900

    Move scatter plot logic to server.R

--- global.R
+++ global.R
@@ -29,36 +29,16 @@ my_data = my_data %>%
 
 str(my_data)
 
-# Second menuItem visualization
-# Create Histogram and boxplot for distribution tabPanel
-
-# # histogram
-# p1 <- my_data %>%
-#   plot_ly() %>%
-#   add_histogram(~get(input$var1)) %>%
-#   layout(xaxis = list(title=paste(input$var1)))
-# 
-# # box plot
-# p2 <- my_data %>%
-#   plot_ly() %>%
-#   add_boxplot(~get(input$var1)) %>%
-#   layout(yaxis = list(showticklabels=F))
-# 
-# #stack plots
-# subplot(p2, p1, nrows =2, shareX = TRUE) %>% hide_legend() %>%
-#   layout(title="Distribution chart - Histogram and Boxplot", 
-#          yaxis = list(title="Frequency"))
-
 #Choices for selectInput - without States column
 c1 = my_data %>% select(-State) %>% names
 
 # Create scatter plot for relationship using ggplot
 
-my_data %>% ggplot(aes(x=Rape, y=Assault)) +
+my_data %>% ggplot(aes(x=get(input$var3), y=get(input$var4) )) +
   geom_point() +
   geom_smooth(method = "lm")+
-  labs(title = "Relation between rape and assault arrests",
-       x="Rape",
-       y="Assault") +
+  labs(title = paste("Relationship between", input$var3, "and", input$var4),
+       x=input$var3,
+       y=input$var4) +
   theme( plot.title = element_textbox_simple(size=10, halign=0.5))
 
