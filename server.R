0e1b681803c76d358bd61b6c306473f106baaef3
todd-m-sinclair <todd.m.sinclair@icloud.com>
2023-03-28 12:39:33 +0900

    Add output$hist, copy logic from global.R

--- server.R
+++ server.R
@@ -18,4 +18,25 @@ function(input, output, session){
   output$dataT <-renderDataTable(
     my_data
   )
+  
+  output$histplot <- renderPlotly(
+    
+    # histogram
+    p1 <- my_data %>%
+      plot_ly() %>%
+      add_histogram(~get(input$var1)) %>%
+      layout(xaxis = list(title=input$var1))
+    
+    # box plot
+    p2 <- my_data %>%
+      plot_ly() %>%
+      add_boxplot(~get(input$var1)) %>%
+      layout(yaxis = list(showticklabels=F))
+    
+    #stack plots
+    subplot(p2, p1, nrows =2, shareX = TRUE) %>% hide_legend() %>%
+      layout(title="Distribution chart - Histogram and Boxplot", 
+             yaxis = list(title="Frequency"))
+    
+  )
 }
