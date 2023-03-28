e4c52177234253d11f0aa215eafe801cce5a04fe
todd-m-sinclair <todd.m.sinclair@icloud.com>
2023-03-28 11:56:28 +0900

    Wrangle data: summary, structure, data for use in server

--- global.R
+++ global.R
@@ -1,1 +1,26 @@
 # Helper file, runs at launch. Accessible to both ui.R and server.R objects
+
+library(tidyverse)
+# USArrests dataset
+
+my_data <- USArrests
+
+# structure of data
+my_data %>% 
+  str()
+
+#summary of data
+my_data %>% 
+  summary()
+
+#first few observations
+my_data %>% 
+  head()
+
+#assign row names to ojbect
+states=rownames(my_data)
+
+#Add column to data set
+my_data = my_data %>% mutate(State=states)
+
+str(my_data)
