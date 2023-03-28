9b98a0b07a5c383e03daa5a20411caa7e106887a
todd-m-sinclair <todd.m.sinclair@icloud.com>
2023-03-28 14:17:56 +0900

    Update readability.

--- server.R
+++ server.R
@@ -1,37 +1,29 @@
-# Computation and reactivity
-
 function(input, output, session){
   
-  # Rendering the box header  
+  # Render the box header for top 5 crime rate  
   output$head1 <- renderText(
     paste("5 states with high rate of", input$var2, "Arrests")
   )
   
-  # Rendering the box header 
+  # Render the box header for low 5 crime rate  
   output$head2 <- renderText(
     paste("5 states with low rate of", input$var2, "Arrests")
   )
   
-  
-  # Rendering table with 5 states with high arrests for specific crime type
+  # Render table with 5 states with high arrests for specific crime type
   output$top5 <- renderTable({
-    
     my_data %>% 
       select(State, input$var2) %>% 
       arrange(desc(get(input$var2))) %>% 
       head(5)
-    
   })
   
-  # Rendering table with 5 states with low arrests for specific crime type
+  # Render table with 5 states with low arrests for specific crime type
   output$low5 <- renderTable({
-    
     my_data %>% 
       select(State, input$var2) %>% 
       arrange(get(input$var2)) %>% 
       head(5)
-    
-    
   })
   
   #Structure
@@ -56,31 +48,31 @@ function(input, output, session){
     # histogram
     p1 <- my_data %>%
       plot_ly() %>%
-      add_histogram(x=~get(input$var1)) %>%
-      layout(xaxis = list(title=paste(input$var1)))
+      add_histogram(x = ~get(input$var1)) %>%
+      layout(xaxis = list(title = paste(input$var1)))
     
     # box plot
     p2 <- my_data %>%
       plot_ly() %>%
-      add_boxplot(x=~get(input$var1)) %>%
-      layout(yaxis = list(showticklabels=F))
+      add_boxplot(x= ~get(input$var1)) %>%
+      layout(yaxis = list(showticklabels = F))
     
     #stack plots
-    subplot(p2, p1, nrows =2, shareX = TRUE) %>% hide_legend() %>%
+    subplot(p2, p1, nrows =2, shareX = TRUE) %>% 
+      hide_legend() %>%
       layout(title="Distribution chart - Histogram and Boxplot", 
              yaxis = list(title="Frequency"))
-    
-  })
+    })
   
   output$scatter <- renderPlotly({
     # Create scatter plot for relationship using ggplot
     
-    p <- my_data %>% ggplot(aes(x=get(input$var3), y=get(input$var4) )) +
+    p <- my_data %>% ggplot(aes(x = get(input$var3), y = get(input$var4) )) +
       geom_point() +
       geom_smooth(method = get(input$fit)) +
       labs(title = paste("Relationship between", input$var3, "and", input$var4),
-           x=input$var3,
-           y=input$var4) +
+           x = input$var3,
+           y = input$var4) +
       theme( plot.title = element_textbox_simple(size=10, halign=0.5))
     
     ggplotly(p)
@@ -105,14 +97,13 @@ function(input, output, session){
     )
     
     ggplotly(corr.plot)
-    
   })
   
   ### Bar Charts - State wise trend
   output$bar <- renderPlotly({
     my_data %>% 
       plot_ly() %>% 
-      add_bars(x=~State, y=~get(input$var2)) %>% 
+      add_bars(x = ~State, y = ~get(input$var2)) %>% 
       layout(title = paste("Statewise Arrests for", input$var2),
              xaxis = list(title = "State"),
              yaxis = list(title = paste(input$var2, "Arrests per 100,000 residents") ))
@@ -121,16 +112,18 @@ function(input, output, session){
   # Choropleth map
   output$map_plot <- renderPlot({
     new_join %>% 
-      ggplot(aes(x=long, y=lat,fill=get(input$crimetype) , group = group)) +
+      ggplot(aes(x = long, y = lat,fill = get(input$crimetype) , group = group)) +
       geom_polygon(color="black", size=0.4) +
-      scale_fill_gradient(low="#73A5C6", high="#001B3A", name = paste(input$crimetype, "Arrest rate")) +
+      scale_fill_gradient(low="#73A5C6", 
+                          high="#001B3A", 
+                          name = paste(input$crimetype, "Arrest rate")) +
       theme_void() +
-      labs(title = paste("Choropleth map of", input$crimetype , " Arrests per 100,000 residents by state in 1973")) +
-      theme(
-        plot.title = element_textbox_simple(face="bold", 
-                                            size=18,
-                                            halign=0.5),
-        
+      labs(title = paste("Choropleth map of", 
+                         input$crimetype , 
+                         "Arrests per 100,000 residents by state in 1973")) +
+      theme(plot.title = element_textbox_simple(face="bold", 
+                                                size=18,
+                                                halign=0.5),
         legend.position = c(0.2, 0.1),
         legend.direction = "horizontal"
         
